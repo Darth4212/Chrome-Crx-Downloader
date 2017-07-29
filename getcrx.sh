884060cc2e54a5e4078ee3abc78430101e8972f9
Cody St Pierre <codystpierre0@gmail.com>
2017-07-29 17:30:58 -0500

    Update getcrx.sh

--- getcrx.sh
+++ getcrx.sh
@@ -9,4 +9,4 @@ if [ "$1" == "-h" ]; then
   exit 0
 fi
 crx="$2"
-wget -U --header="$USER" "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D$extensionid%26installsource%3Dondemand%26uc" -O ~/Downloads/$crx
+wget -U --header="$USER" "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D$extensionid%26installsource%3Dondemand%26uc" -O ~/Downloads/$crx.crx
